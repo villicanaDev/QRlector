ee0fc48544072f7096fd57481a186d9d09e62399
Oscar Villicaña (Aqua) <106110521+villicanaDev@users.noreply.github.com>
2025-03-03 10:09:35 -0600

    Fix | index

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Qr lector Emtech TEST</title>
+    <title>Qr lector Emtech TEST para ver si jalan las CD/CI</title>
 </head>
 <body>
 
